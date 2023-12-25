8cd8ccc1d27d1933744e99ac8d9ae8d9ae2f0f77
Rahul Singh Takuli <rahulsinghkb56@gmail.com>
2023-12-25 22:53:57 +0530

    update

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,22 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/about-me.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+            Passionate Full-Stack Developer with a proven track record of delivering diverse web development projects encompassing both front-end and back-end solutions.
+
+          </p>
+          <p className="hero--section-description">
+            Proficient in a wide array of technologies, including HTML, CSS, Tailwind CSS, Bootstrap, JavaScript, jQuery, Node.js, Express.js, SQL, MongoDB, GitHub, and intermediate proficiency in React.js. Well-versed in data structures and algorithms, with a strong foundation in C/C++. Committed to continuous learning and staying at the forefront of evolving technologies.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
