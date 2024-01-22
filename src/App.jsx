66bb8b587c43f1bcc4782e626b090b0eef4000d2
Rahul Singh Takuli <118590603+rahul-singh-takuli56@users.noreply.github.com>
2024-01-22 18:24:50 +0530

    to-do-application

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import Boards from './Components/Boards'
+
+const App = () => {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquare = history[currentMove];
+
+  const handlePlay = (nextSquare) => {
+    const nextHistoy = [...history.slice(0, currentMove + 1), nextSquare];
+    setHistory(nextHistoy);
+    setCurrentMove(nextHistoy.length - 1);
+  }
+
+  const jumpTo = (nextMove) => {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = `Go to move # ${move}`;
+    }
+    else {
+      description = "Let's Start Game";
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    )
+  })
+
+  return (
+    <div className='flex h-screen w-full justify-center items-center  gap-8'>
+      <div className='Board'>
+        <Boards xIsNext={xIsNext} squares={currentSquare} onPlay={handlePlay} />
+      </div>
+      <div>
+        <ol>
+          {moves}
+        </ol>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Boards.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import Square from './Square';
+import CalculateWinner from '../Utility/CalculateWinner';
+
+const Boards = ({ xIsNext, squares, onPlay }) => {
+    const handleClick = (i) => {
+        if (CalculateWinner(squares) || squares[i]) {
+            return;
+        }
+        const nextSquare = squares.slice();
+        console.log(nextSquare);
+        if (xIsNext) {
+            nextSquare[i] = "X";
+        }
+        else {
+            nextSquare[i] = "0";
+        }
+        onPlay(nextSquare);
+    }
+
+    const winner = CalculateWinner(squares);
+    let status;
+    if (winner) {
+        status = "Winner: " + winner;
+    }
+    else {
+        status = "Next Player: " + (xIsNext ? "X" : "0");
+    }
+
+    return (
+        <>
+            <h2 className='mb-2 text-lg'>{status}</h2>
+            <div className='flex'>
+                <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+                <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+                <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+            </div>
+            <div className='flex'>
+                <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+                <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+                <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+            </div>
+            <div className='flex'>
+                <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+                <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+                <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+            </div>
+        </>
+    )
+}
+
+export default Boards;
